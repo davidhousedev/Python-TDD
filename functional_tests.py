7662a4e33f6af5ffafbc2977626078ced515c11b
David House <davidhousedev@gmail.com>
2017-01-30 19:55:59 -0800

    docs: Add user story for basic function tests

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,29 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
     firefox_path='/usr/bin/firefox'
 ))
+
+# Wilson opens his browser to his favorite to-do list app
 browser.get('http://localhost:8000')
 
+# He notices that he has reached the proper page
 assert 'Django' in browser.title
+
+# He has the opportunity to create a to-do list item
+
+# Like any obedient developer, he creates a reminder to:
+# 'Continue obeying the Test Goat'
+
+# When he hits enter, the page updates and now lists his new
+# to-do: '1: Continue obeying the Test Goat' as a to-do item
+
+# There is still a text box inviting Wilson to add another item
+# so he adds 'Step carefully along the cliff's wall'
+
+# The page updates again, displaying both items on the list
+
+# Wilson is presented with a URL unique to his list
+
+# He visits the provided URL, and notices that his list is
+# still there
+
+# Satisfied, he closes the browser window
