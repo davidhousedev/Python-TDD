ac2e74d39a0236f9496a159273cc27bc11b5b925
David House <davidhousedev@gmail.com>
2017-01-30 20:03:16 -0800

    fix: Changed Firefox behavior to standard approach in tutorial

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
-    firefox_path='/usr/bin/firefox'
-))
+browser = webdriver.Firefox()
 
 # Wilson opens his browser to his favorite to-do list app
 browser.get('http://localhost:8000')
