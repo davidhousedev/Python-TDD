aa504e231133c2f4a5ba450f9b4f52f2746db340
David House <davidhousedev@gmail.com>
2017-02-01 11:50:58 -0800

    feat: Expand functional test to check whether user can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -15,18 +18,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices that the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He has the opportunity to create a to-do list item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# Like any obedient developer, he creates a reminder to:
-# 'Continue obeying the Test Goat'
+        # Like any obedient developer, he creates a reminder to:
+        # 'Obey the Testing Goat'
+        inputbox.send_keys('Obey the Testing Goat')
 
-# When he hits enter, the page updates and now lists his new
-# to-do: '1: Continue obeying the Test Goat' as a to-do item
+        # When he hits enter, the page updates and now lists his new
+        # to-do: '1: Continue obeying the Test Goat' as a to-do item
+        inputbox.send_keys(Keys.ENTER)
 
-# There is still a text box inviting Wilson to add another item
-# so he adds 'Step carefully along the cliff's wall'
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Obey the Testing Goat' for row in rows)
+        )
+
+        # There is still a text box inviting Wilson to add another item
+        # so he adds 'Step carefully along the cliff's wall'
+        self.fail('Finish the test!')
 
 # The page updates again, displaying both items on the list
 
