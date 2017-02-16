e7a3b5392ea363160f6615c5ec93bd14759e6283
David House <davidhousedev@gmail.com>
2017-02-16 10:22:14 -0800

    refactor: Remove "voodoo" sleeps by creating a retry loop in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -14,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Wilson opens his browser to his favorite to-do list app
@@ -42,20 +52,18 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates and now lists his new
         # to-do: '1: Continue obeying the Test Goat' as a to-do item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Obey the Testing Goat')
+        self.wait_for_row_in_list_table('1: Obey the Testing Goat')
 
         # There is still a text box inviting Wilson to add another item
         # so he adds 'Step carefully along the cliff's wall'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Step carefully along the cliff wall')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, displaying both items on the list
-        self.check_for_row_in_list_table('1: Obey the Testing Goat')
-        self.check_for_row_in_list_table('2: Step carefully along the cliff wall')
+        self.wait_for_row_in_list_table('1: Obey the Testing Goat')
+        self.wait_for_row_in_list_table('2: Step carefully along the cliff wall')
 
         # Wilson is presented with a URL unique to his list
         self.fail('Finish the test!')
